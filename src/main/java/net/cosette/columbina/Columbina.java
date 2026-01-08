702c017de0a6c8966a199af715c417bf52418712
cosette <cosettesys@gmail.com>
2026-01-08 16:41:10 +0100

    Start

---
+++ src/main/java/net/cosette/columbina/Columbina.java
@@ -0,0 +1,16 @@
+package net.cosette.columbina;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Columbina implements ModInitializer {
+	public static final String MOD_ID = "columbina";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/cosette/columbina/ColumbinaClient.java
@@ -0,0 +1,10 @@
+package net.cosette.columbina;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColumbinaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
